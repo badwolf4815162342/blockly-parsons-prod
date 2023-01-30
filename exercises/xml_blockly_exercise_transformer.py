d51ced6ccec37bb47dd22b158bffa177b1220440
ge82fum <ge82fum@forstudents.onmicrosoft.com>
2023-01-30 03:02:02 +0000

    changes

--- exercises/xml_blockly_exercise_transformer.py
+++ exercises/xml_blockly_exercise_transformer.py
@@ -8,9 +8,6 @@ path = "./original_xml"
 dir_list = os.listdir(path)
  
 print("Files and directories in '", path, "' :")
- 
-# prints all files
-print(dir_list)
 
 for filename in dir_list:
     f = open('./original_xml/'+filename, "r")
@@ -19,9 +16,6 @@ for filename in dir_list:
     f.write(original_xml.replace('`','-'))
     f.close()
 
-    #original_xml.replace("bock ", "block editable="false" deletable="false" ")
-    #original_xml.replace("shadow ", "shadow editable="false" ")
-
     mytree = ET.parse('./xml/'+filename)
     myroot = mytree.getroot()
     counter1 = 0
@@ -29,34 +23,48 @@ for filename in dir_list:
     for toplevelelement in myroot:
         if '}block' in toplevelelement.tag:
             counter1 += 1
+            # Make lower level BLOCK-blocks not editable or deletable
+            #original_xml.replace("bock ", "block editable="false" deletable="false" ")
+            #original_xml.replace("shadow ", "shadow editable="false" ")
             toplevelelement.set('editable', 'false')
             toplevelelement.set('deletable', 'false')
+            toplevelelement.set('contextMenu','false')
+            toplevelelement.set('enableContextMenu','false')
+            # Make all FIELD-blocks that supposed to be headlines to be seen as such
             for child in toplevelelement.findall('.//{https://developers.google.com/blockly/xml}field'):
-                #print(child.tag)
                 if child.text != None:
+                    # Headline or Grouptitle comments as new block called group_title
                     if 'Title: ' in child.text:
-                        ## print('Text '+child.text)
                         count += 1
                         child.text = child.text.replace('Title: ','')
                         toplevelelement.set('type','group_title')
-                        toplevelelement.set('movable','false')
-        #for secondlevel in toplevelelement:
-            #print(secondlevel)
-        #    for thirdlevel in secondlevel:
-                #print(thirdlevel)
+                        # Make Headline or Grouptitle comments movable
+                        # toplevelelement.set('movable','false')
 
     counter2 = 0
+    # set all top level BLOCK-blocks that are editable to be not movable or editable or deletable
     for block in myroot.findall('.//{https://developers.google.com/blockly/xml}block'):
         if block.get('editable') != 'false':
             counter2 += 1
             block.set('movable','false')
             block.set('editable', 'false')
             block.set('deletable', 'false')
+            block.set('contextMenu','false')
+            block.set('enableContextMenu','false')
 
+    for block in myroot.findall('.//{https://developers.google.com/blockly/xml}field'):
+        block.set('contextMenu','false')
+        block.set('enableContextMenu','false')
+
+    for block in myroot.findall('.//{https://developers.google.com/blockly/xml}value'):
+        block.set('contextMenu','false')
+        block.set('enableContextMenu','false')
     
-
+    # set all shadow-blocks in the lower levels to not be deletable
     for shadow in myroot.findall('.//{https://developers.google.com/blockly/xml}shadow'):
         shadow.set('deletable', 'false')
+        shadow.set('contextMenu','false')
+        shadow.set('enableContextMenu','false')
 
     print('renamed blocks '+str(count))
     print(counter1)
@@ -73,6 +81,7 @@ for filename in dir_list:
     transformed_f.close()
     # BasetitleXML    
 
+    # Create only titles Files
     mytree = ET.parse('./xml/'+filename)
     myroot = mytree.getroot()
     def iterator(parents, nested=False):
@@ -81,10 +90,21 @@ for filename in dir_list:
                 if len(child) >= 1:
                     iterator(child)
             if child.attrib.get('type', None) != None:
+                parents.set('contextMenu','false')
+                child.set('contextMenu','false')
+                parents.set('enableContextMenu','false')
+                child.set('enableContextMenu','false')
+
                 if 'group' not in child.attrib['type']:  # Add your entire condition here
                     parents.remove(child)
+                else:
+                    # Make Headline or Grouptitle comments movable
+                    parents.set('movable','false')
+                    child.set('movable','false')
+
 
     iterator(myroot, nested=True)
+
     mytree.write('./xml_titles/'+filename)
     transformed_titles_f = open('./xml_titles/'+filename, "r")
     transformed_titles_xml = transformed_titles_f.read()
