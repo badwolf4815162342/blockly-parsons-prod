91ddbf36e3d53d1b44ad7be4636334999cbf13f6
ge82fum <ge82fum@forstudents.onmicrosoft.com>
2023-02-04 16:07:14 +0000

    changes

--- exercises/xml_blockly_exercise_transformer.py
+++ exercises/xml_blockly_exercise_transformer.py
@@ -2,6 +2,32 @@
 import os
 import xml.etree.ElementTree as ET
 
+def makeChildrenUneditable(element):
+    children = reversed(element)
+    for child in children:
+        # print("child found")
+        child.set('editable', 'false')
+        child.set('movable','false')
+        makeChildrenUneditable(child)
+    return
+
+def setFilteredChildrenEditableValue(value, element, filterString):
+    children = reversed(element)
+    for child in children:
+        if filterString in child.tag:
+            child.set('editable', value)
+            child.set('movable',value)
+        setFilteredChildrenEditableValue(value, child, filterString)
+    return
+
+def setFilteredChildrenByParentEditableValue(value, element, filterString):
+    children = reversed(element)
+    for child in children:
+        if filterString in element.tag:
+            child.set('editable', value)
+            child.set('movable',value)
+        setFilteredChildrenByParentEditableValue(value,child, filterString)
+    return
  
 # Get the list of all files and directories
 path = "./original_xml"
@@ -10,6 +36,7 @@ dir_list = os.listdir(path)
 print("Files and directories in '", path, "' :")
 
 for filename in dir_list:
+    print(filename)
     f = open('./original_xml/'+filename, "r")
     original_xml = f.read()
     f = open('./xml/'+filename, "w")
@@ -18,11 +45,9 @@ for filename in dir_list:
 
     mytree = ET.parse('./xml/'+filename)
     myroot = mytree.getroot()
-    counter1 = 0
-    count = 0
+
     for toplevelelement in myroot:
         if '}block' in toplevelelement.tag:
-            counter1 += 1
             # Make lower level BLOCK-blocks not editable or deletable
             #original_xml.replace("bock ", "block editable="false" deletable="false" ")
             #original_xml.replace("shadow ", "shadow editable="false" ")
@@ -35,47 +60,64 @@ for filename in dir_list:
                 if child.text != None:
                     # Headline or Grouptitle comments as new block called group_title
                     if 'Title: ' in child.text:
-                        count += 1
                         child.text = child.text.replace('Title: ','')
                         toplevelelement.set('type','group_title')
+                        y = int(toplevelelement.get('y'))
+                        toplevelelement.set('y',str(y+30))
                         # Make Headline or Grouptitle comments movable
                         # toplevelelement.set('movable','false')
+                        makeChildrenUneditable(toplevelelement)
+            # edit disabled piece to be own piece
+            if toplevelelement.get('disabled') == 'true':
+                toplevelelement.set('editable', 'true')
+                # print('disabled found')
+                toplevelelement.set('type','disabled_loop')
+                toplevelelement.set('disabled','false')
+                for secondlevelelement in reversed(toplevelelement):
+                    # if not the statement (inner block) delete
+                    if '}statement' not in secondlevelelement.tag:
+                        #   if child.get('name') == 'TIMES':
+                        toplevelelement.remove(secondlevelelement)
+                    # if secondlevelelement is statement inner block
+                    else:
+                        for statment_child in reversed(secondlevelelement):
+                            if '}block' in statment_child.tag:
+                                statment_child.set('disabled','false')
+                                statment_child.set('editable', 'true')
+                                statment_child.set('movable','true')
+                                for block_children in reversed(statment_child):
+                                    makeChildrenUneditable(block_children)
+                        # set everything that is a child of a statement editable (and also the statements)
+                        setFilteredChildrenEditableValue('true', toplevelelement,'}statement')
+                        setFilteredChildrenByParentEditableValue('true', toplevelelement,'}statement')
+                        # make the appending next bit of code editable
+                        setFilteredChildrenEditableValue('true', toplevelelement,'}next')
+                        setFilteredChildrenByParentEditableValue('true', toplevelelement,'}next')
 
-    counter2 = 0
-    # set all top level BLOCK-blocks that are editable to be not movable or editable or deletable
-    for block in myroot.findall('.//{https://developers.google.com/blockly/xml}block'):
-        if block.get('editable') != 'false':
-            counter2 += 1
-            block.set('movable','false')
-            block.set('editable', 'false')
-            block.set('deletable', 'false')
-            block.set('contextMenu','false')
-            block.set('enableContextMenu','false')
 
-    for block in myroot.findall('.//{https://developers.google.com/blockly/xml}field'):
-        block.set('contextMenu','false')
-        block.set('enableContextMenu','false')
 
-    for block in myroot.findall('.//{https://developers.google.com/blockly/xml}value'):
-        block.set('contextMenu','false')
-        block.set('enableContextMenu','false')
-    
-    # set all shadow-blocks in the lower levels to not be deletable
-    for shadow in myroot.findall('.//{https://developers.google.com/blockly/xml}shadow'):
-        shadow.set('deletable', 'false')
-        shadow.set('contextMenu','false')
-        shadow.set('enableContextMenu','false')
+            # for everything that is not in the disabled bit
+            else:
+                # set shadows fields and values uneditable
+                setFilteredChildrenEditableValue('false', toplevelelement,'}shadow')
+                setFilteredChildrenEditableValue('false', toplevelelement,'}field')
+                setFilteredChildrenEditableValue('false', toplevelelement,'}value')
+                setFilteredChildrenEditableValue('false', toplevelelement,'}block')
+                # set everything that is a child of a statement editable (and also the statements)
+                setFilteredChildrenEditableValue('true', toplevelelement,'}statement')
+                setFilteredChildrenByParentEditableValue('true', toplevelelement,'}statement')
+                # make the appending next bit of code editable
+                setFilteredChildrenEditableValue('true', toplevelelement,'}next')
+                setFilteredChildrenByParentEditableValue('true', toplevelelement,'}next')
 
-    print('renamed blocks '+str(count))
-    print(counter1)
-    print(counter2)
+
 
     mytree.write('./xml/'+filename)
     transformed_f = open('./xml/'+filename, "r")
     # print(f.read())
     transformed_xml = transformed_f.read()
     transformed_f.close()
-    final_xml = transformed_xml.replace('ns0:','').replace(':ns0','').replace('disabled="true"','disabled="true" movable="false"')
+    final_xml = transformed_xml.replace('ns0:','').replace(':ns0','') #.replace('disabled="true"','disabled="true" movable="false"')
     transformed_f = open('./xml/'+filename, "w")
     transformed_f.write(final_xml)
     transformed_f.close()
@@ -99,8 +141,8 @@ for filename in dir_list:
                     parents.remove(child)
                 else:
                     # Make Headline or Grouptitle comments movable
-                    parents.set('movable','false')
-                    child.set('movable','false')
+                    parents.set('movable','true')
+                    child.set('movable','true')
 
 
     iterator(myroot, nested=True)
